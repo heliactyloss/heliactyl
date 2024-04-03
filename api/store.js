554971e3dea37b0b4e8e90a66ff38086163ec007
achul123 <73477238+achul123@users.noreply.github.com>
2024-04-03 18:25:47 +0200

    Update store.js

--- api/store.js
+++ api/store.js
@@ -66,7 +66,7 @@ module.exports.load = async function (app, db) {
     adminjs.suspend(req.session.userinfo.id);
 
     log(
-      `Resources Purchased`,
+      `resources purchased`,
       `${req.session.userinfo.username}#${req.session.userinfo.discriminator} bought ${extraResource} ${type} from the store for \`${purchaseCost}\` coins.`
     );
 
